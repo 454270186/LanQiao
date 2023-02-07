537b9e598a21574e7fdc090b76640ac3a0bf2bd1
454270186 <sc21ey@leeds.ac.uk>
2023-02-08 00:31:46 +0800

    22-C

---
+++ 2022/C.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+const int N = 2e5 + 5;
+
+int main() {
+    long long nums[N];
+    long long sum[N];
+
+    int n = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> nums[i];
+        sum[i] = sum[i-1] + nums[i];
+    }
+
+    long long ans = 0;
+    for (int i = 1; i <= n; i++) {
+        ans += nums[i] * (sum[n] - sum[i]);
+    }
+
+    cout << ans << endl;
+    return 0;
+}
